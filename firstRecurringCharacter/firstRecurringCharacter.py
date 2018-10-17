e10256f0f31906203927c33db37e871f13096ad9
Sam Rapier <samrapier@live.co.uk>
2018-10-17 13:37:12 +0100

    added code to the first recurring character program

---
+++ firstRecurringCharacter/firstRecurringCharacter.py
@@ -0,0 +1,34 @@
+def firstRecurring(givenString):
+	"""
+	Return the first recurring character in a string that is passed in
+
+	e.g.    ABCA    - should return A
+			BCABA   - should return B
+			ABC     - should return null
+
+
+	Both of the loops do the same thing however, one uses a list, the
+	other a dictionary
+	"""
+	charArr = []
+	charDict = {}
+
+
+
+	# loop through the string
+	for char in givenString:
+		# for each letter, check if the letter is already in the list 
+		# and return it if it is
+		if char in charArr:
+			return char
+		
+		# otherwise add that letter to the list
+		charArr.append(char)
+
+
+	for char in givenString:
+		if char in charDict:
+			return char
+		charDict[char] = 1
+
+print(firstRecurring('DBCBA'))
